1c0cdf2a78428757106aa45623ddc26f4c68afed
Rajiv Senthilnathan <rsenthil@redhat.com>
2020-05-25 19:25:13 -0400

    Add memberstatus and toolchainstatus

---
+++ pkg/apis/toolchain/v1alpha1/memberstatus_types.go
@@ -0,0 +1,60 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MemberStatusSpec defines the desired state of MemberStatus
+// +k8s:openapi-gen=true
+type MemberStatusSpec struct {
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// spec intentionally left empty since only the status fields will be used for reporting status of the toolchain
+}
+
+// MemberStatusStatus defines the observed state of the overall toolchain status
+// +k8s:openapi-gen=true
+type MemberStatusStatus struct {
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Conditions is an array of current toolchain status conditions
+	// Supported condition types: ConditionReady
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType
+	Conditions []Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MemberStatus is used to track overall toolchain status
+// +k8s:openapi-gen=true
+// +kubebuilder:resource:scope=Namespaced
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=`.status.conditions[?(@.type=="Ready")].reason`
+// +kubebuilder:validation:XPreserveUnknownFields
+// +operator-sdk:gen-csv:customresourcedefinitions.displayName="CodeReady Toolchain Status"
+type MemberStatus struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MemberStatusSpec   `json:"spec,omitempty"`
+	Status MemberStatusStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MemberStatusList contains a list of MemberStatus
+type MemberStatusList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MemberStatus `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MemberStatus{}, &MemberStatusList{})
+}
---
+++ pkg/apis/toolchain/v1alpha1/toolchainstatus_types.go
@@ -0,0 +1,60 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ToolchainStatusSpec defines the desired state of ToolchainStatus
+// +k8s:openapi-gen=true
+type ToolchainStatusSpec struct {
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// spec intentionally left empty since only the status fields will be used for reporting status of the toolchain
+}
+
+// ToolchainStatusStatus defines the observed state of the overall toolchain status
+// +k8s:openapi-gen=true
+type ToolchainStatusStatus struct {
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Conditions is an array of current toolchain status conditions
+	// Supported condition types: ConditionReady
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType
+	Conditions []Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ToolchainStatus is used to track overall toolchain status
+// +k8s:openapi-gen=true
+// +kubebuilder:resource:scope=Namespaced
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=`.status.conditions[?(@.type=="Ready")].reason`
+// +kubebuilder:validation:XPreserveUnknownFields
+// +operator-sdk:gen-csv:customresourcedefinitions.displayName="CodeReady Toolchain Status"
+type ToolchainStatus struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ToolchainStatusSpec   `json:"spec,omitempty"`
+	Status ToolchainStatusStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ToolchainStatusList contains a list of ToolchainStatus
+type ToolchainStatusList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ToolchainStatus `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ToolchainStatus{}, &ToolchainStatusList{})
+}
